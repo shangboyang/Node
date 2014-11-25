88eee0789b5cbec6e0b369dee325345a285e4b82
shangboyang <yang8701@gmail.com>
2014-11-25 20:29:46 +0800

    hello

---
+++ node_0010_helloworld/server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer(function (req, resp) {
+	
+	resp.writeHead(200, {"Content-Type": "text/plain"});
+	
+	resp.end("Hello World\n");
+
+
+}).listen(3000, "127.0.0.1");
+
+console.log("Server running at http://127.0.0.1:3000~");
